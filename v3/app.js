de3721bcfa3dc7f808f07fbfe13ae806c0072076
Yuki Ou <yukk1ou@hotmail.com>
2018-11-25 20:04:56 -0800

    users + comments fixed

--- v3/app.js
+++ v3/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 // __dirname refers to the directory that this scrip is running
 app.use(express.static(__dirname + "/public"))
-seedDB();
+// seedDB();  // seed the db   
 
 // PASSPORT config
 app.use(require("express-session")({
